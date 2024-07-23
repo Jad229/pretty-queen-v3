36bbb42711d3a47263c7e358c00a3c312c0da5a5
Jad229 <jad229@njit.edu>
2024-07-23 05:30:54 -0400

    spaced menu items and added gradient

--- src/components/MobileNavigation/MobileNavigation.jsx
+++ src/components/MobileNavigation/MobileNavigation.jsx
@@ -2,11 +2,20 @@ import styles from "./MobileNavigation.module.css";
 
 export default function MobileNavigation() {
   return (
-    <div className={styles.mobileNavContainer}>
-      <a href="/">Home</a>
-      <a href="/products">Products</a>
-      <a href="/services">Services</a>
-      <a href="/about">About Us</a>
+    <div className={styles.mobileMenuContainer}>
+      <a className={styles.menuItem} href="/">
+        Home
+      </a>
+      <a className={styles.menuItem} href="/products">
+        Products
+      </a>
+      <a className={styles.menuItem} href="/services">
+        Services
+      </a>
+      <a className={styles.menuItem} href="/about">
+        About Us
+      </a>
+      <button className={styles.button}>Book Now</button>
     </div>
   );
 }
