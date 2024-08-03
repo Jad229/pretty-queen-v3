57b05a3ba8378a50c57ff9157024016c9e90ac92
Jad229 <jad229@njit.edu>
2024-08-03 17:24:32 -0400

    mobile layout implemented

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,23 +1,27 @@
-import Button from "../ui/Button/Button";
 import Hero from "../../assets/hero.png";
 import styles from "./Landing.module.css";
 
 export default function Landing() {
   return (
     <section className={styles.section}>
-      <h2 className={styles.sectionHeading}>
-        A <span className={styles.headingRoyal}>Royal</span> Experience Every
-        Visit
-      </h2>
-      <p className={styles.sectionParagraph}>
-        Bring out and embrace your inner queen
-      </p>
-      <Button className={styles.btn}>Explore Shop</Button>
-      <img
-        className={styles.heroImage}
-        src={Hero}
-        alt="woman with hair flowing"
-      />
+      <div className={styles.textContainer}>
+        <h1 className={styles.sectionHeading}>
+          A <span className={styles.headingRoyal}>Royal</span> Experience Every
+          Visit
+        </h1>
+        <p className={styles.sectionParagraph}>
+          Bring out and embrace your inner queen
+        </p>
+        <button className={styles.btn}>Explore Shop</button>
+      </div>
+      <div className={styles.heroContainer}>
+        <div className={styles.halo}></div>
+        <img
+          className={styles.heroImage}
+          src={Hero}
+          alt="woman with hair flowing"
+        />
+      </div>
     </section>
   );
 }
