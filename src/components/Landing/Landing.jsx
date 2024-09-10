0a26251ab7cc01753e7c1826f13e9fbf0b3ede0f
Jad229 <jad229@njit.edu>
2024-09-09 21:42:30 -0400

    Finished landing page redesign

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,6 +1,15 @@
-import Hero from "../../assets/hero.png";
+import Hero from "../../assets/hero.svg";
 import styles from "./Landing.module.css";
 
 export default function Landing() {
-  return <section className={styles.section}></section>;
+  return (
+    <section className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.textLeft}>Pretty</h1>
+        <img className={styles.heroImage} src={Hero} alt="hero image" />
+        <h1 className={styles.textRight}>Queen</h1>
+      </div>
+      <button className={styles.btn}>Explore More</button>
+    </section>
+  );
 }
