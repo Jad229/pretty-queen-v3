3ac4459c6c693f8db479c884bde0dbfaf15d42fd
Jad229 <jad229@njit.edu>
2024-09-15 17:07:44 -0400

    initial html for product page

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -9,6 +9,32 @@ export default function ProductPage() {
         <Image src={Hero.src} width={200} height={400} alt="hero image" />
         <h2 className={styles.textRight}>Queen</h2>
       </div>
+      <div className={styles.filterBar}>
+        <ul className={styles.filterLeft}>
+          <li>
+            <button>All Products</button>
+          </li>
+          <li>
+            <button>Hair</button>
+          </li>
+          <li>
+            <button>Shampoo</button>
+          </li>
+        </ul>
+        <ul className={styles.filterRight}>
+          <li>
+            <button>Filters</button>
+          </li>
+        </ul>
+      </div>
+      <div className={styles.productsContainer}>
+        <div className={styles.productCard}>
+          <div>PRODUCT IMAGE </div>
+          <h3>Product Title</h3>
+          <p>Product Cost</p>
+          <button>BUY PRODUCT</button>
+        </div>
+      </div>
     </section>
   );
 }
